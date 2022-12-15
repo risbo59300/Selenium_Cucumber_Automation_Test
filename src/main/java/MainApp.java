2a005889308b9c96779e5d865931d56f43945062
GABA-MANO <ybgabamano@gmail.com>
2022-12-15 19:48:31 +0100

    Reccuperation du titre et comparaison.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,41 +1,37 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 
 public class MainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.wikipedia.org");
 
-        //Locators
-        //id
-        Long start = System.currentTimeMillis();
-        driver.findElement(By.id("js-link-box-fr"));
-        Long end = System.currentTimeMillis();
+        WebElement titleOfWebPage = driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-textlogo > h1 > span"));
 
-        System.out.println("The time needed to get a locator by ID is : " + (end - start));
+        String titleText = titleOfWebPage.getText();
+        String expectedText = "Wikipedia";
 
-        //Xpath
-        Long start1 = System.currentTimeMillis();
-        driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[2]/div[2]"));
-        Long end1 = System.currentTimeMillis();
-
-        System.out.println("The time needed to get a locator by Xpath is : " + (end1 - start1));
-
-        //Css
-        Long start2 = System.currentTimeMillis();
-        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang2"));
-        Long end2 = System.currentTimeMillis();
-
-        System.out.println("The time needed to get a locator by CssSelector is : " + (end2 - start2));
+        if (titleText.equals(expectedText)) {
+            System.out.println("Test has passed !");
+        }
+        else {
+            System.out.println("Test did not pass !");
+            driver.close();
+            throw new Exception();
+        }
 
         driver.close();
 
